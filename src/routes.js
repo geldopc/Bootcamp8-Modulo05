5fbccae8aca0ec6e673b094342eaaac29324d398
geldopc <geldopc@gmail.com>
2019-09-14 19:49:59 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository/:repository" exact component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
